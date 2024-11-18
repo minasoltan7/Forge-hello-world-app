f315948d3a5882bb933c51332551a8035061c0d6
Mina Soultan <minasoltan@windowslive.com>
2024-11-18 23:10:10 +0200

    Number of comments added

--- Forge-First-App/src/frontend/index.jsx
+++ Forge-First-App/src/frontend/index.jsx
@@ -1,17 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import ForgeReconciler, { Text } from '@forge/react';
-import { invoke } from '@forge/bridge';
+import React from 'react';
+import ForgeReconciler, { Text, useProductContext } from '@forge/react';
+import { requestJira } from '@forge/bridge';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    invoke('getText', { example: 'my-invoke-variable' }).then(setData);
-  }, []);
+  const context = useProductContext();
+
+  // add these code to keep track of comments
+  const [comments, setComments] = React.useState();
+  console.log(`Number of comments on this issue: ${comments?.length}`);
+
+  const fetchCommentsForIssue = async () => {
+    // extract issue ID instead expecting one from function input
+    const issueId = context?.extension.issue.id;
+  
+    // modify to take issueId variable
+    const res = await requestJira(`/rest/api/3/issue/${issueId}/comment`);
+    const data = await res.json();
+    return data.comments;
+  };
+
+  React.useEffect(() => {
+    if (context) {
+      // extract issue ID from the context
+      const issueId = context.extension.issue.id;
+
+      fetchCommentsForIssue().then(setComments);
+    }
+  }, [context]);
+  console.log (comments)
   return (
     <>
-      <Text>Hello Mina enta gamed gedan</Text>
-      <Text>{data ? data : 'Loading...'}</Text>
+      This is the number of comments in this issue : {comments?.length}
     </>
+    
   );
 };
 
